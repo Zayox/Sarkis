385447c2b2e9a00bdbfc9f28dd5e02088f374ed3
amirmoumen <amirmoumen06@yahoo.fr>
2022-07-02 03:56:44 -0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+let count = 2;
+
+
+function leftCarroussel(){
+  let leftArt1 = document.getElementById("art1").style.left;
+  let leftArt2 = document.getElementById("art2").style.left;
+  let leftArt3 = document.getElementById("art3").style.left;
+  document.getElementById(`round${count-1}`).style.backgroundColor = "black";
+  document.getElementById(`round${count}`).style.backgroundColor = "transparent";
+  document.getElementById(`round${count}`).style.border = "solid black 1px";
+
+  document.getElementById("art1").style.left = parseInt(leftArt1)-130 + "vw";
+  document.getElementById("art2").style.left = parseInt(leftArt2)-85 + "vw";
+  document.getElementById("art3").style.left = parseInt(leftArt3)-130 + "vw";
+
+  document.getElementById("art2").style.transitionDuration = 2.5 + "s";
+
+  count--;
+}
+
+function rightCarroussel(){
+  let leftArt1 = document.getElementById("art1").style.left;
+  let leftArt2 = document.getElementById("art2").style.left;
+  let leftArt3 = document.getElementById("art3").style.left;
+  document.getElementById(`round${count+1}`).style.backgroundColor = "black";
+  document.getElementById(`round${count}`).style.backgroundColor = "transparent";
+  document.getElementById(`round${count}`).style.border = "solid black 1px";
+
+  document.getElementById("art1").style.left = parseInt(leftArt1)+130 + "vw";
+  document.getElementById("art2").style.left = parseInt(leftArt2)+85 + "vw";
+  document.getElementById("art3").style.left = parseInt(leftArt3)+130 + "vw";
+
+  document.getElementById("art2").style.transitionDuration = 1.5 + "s";
+  count++;
+}
